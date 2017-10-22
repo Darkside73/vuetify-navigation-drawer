d9b8c2c6f5977844fabe12fee1e394f1c063bf32
darkside <andrey.garbuz@gmail.com>
2017-10-22 20:50:33 +0300

    move out v-app and drawer to nuxt layout

--- components/MyPage.vue
+++ components/MyPage.vue
@@ -1,23 +1,5 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer enable-resize-watcher persistent app dark v-model="drawer">
-      <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+  <div>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-btn
@@ -57,7 +39,7 @@
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
-  </v-app>
+  </div>
 </template>
 
 <script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,46 @@
 <template>
-  <nuxt></nuxt>
+  <v-app dark>
+    <v-navigation-drawer enable-resize-watcher persistent app dark v-model="drawer">
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <nuxt></nuxt>
+  </v-app>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ]
+    }
+  },
+  computed: {
+    drawer: {
+      get () {
+        return this.$store.state.sidebar
+      },
+      set (val) {
+        this.$store.commit('vuetify/SIDEBAR', val)
+      }
+    }
+  }
+}
 </script>
